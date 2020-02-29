d01e57fba5c464841594d7183173be2cfef4189f
camila-moscibrocki <camilamoscibrocki@gmail.com>
2020-02-29 13:07:47 -0300

    Atualização de views

--- cursodjango/aula4/views.py
+++ cursodjango/aula4/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'aula4/index.html')
+    context = {"alunos": ["Pipoca", "Steven", "Channel", "Chiffon", ]}
+    return render(request, 'aula4/index.html', context=context)
